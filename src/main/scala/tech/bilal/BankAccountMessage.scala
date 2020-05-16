a8510adbb98b7fe165245f301ab5b166a424b5b7
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2020-05-17 01:43:54 +0530

    add example for spawn protocol

--- src/main/scala/tech/bilal/BankAccountMessage.scala
+++ src/main/scala/tech/bilal/BankAccountMessage.scala
@@ -1,7 +1,9 @@
 package tech.bilal
 
+import akka.actor.typed.ActorRef
+
 sealed trait BankAccountMessage
 
-case class Deposit(amount: Int)  extends BankAccountMessage
-case class Withdraw(amount: Int) extends BankAccountMessage
-case object PrintBalance         extends BankAccountMessage
+case class Deposit(amount: Int)               extends BankAccountMessage
+case class Withdraw(amount: Int)              extends BankAccountMessage
+case class GetBalance(replyTo: ActorRef[Int]) extends BankAccountMessage
--- src/main/scala/tech/bilal/Main.scala
+++ src/main/scala/tech/bilal/Main.scala
@@ -1,26 +1,49 @@
 package tech.bilal
 
+import akka.actor.typed.SpawnProtocol.Spawn
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Props, Scheduler, SpawnProtocol}
+import akka.actor.typed.scaladsl.AskPattern.Askable
+import akka.util.Timeout
+
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 object Main extends App {
 
   def behavior(balance: Int): Behavior[BankAccountMessage] = Behaviors.receiveMessage {
     case Deposit(amount)  => behavior(balance + amount)
     case Withdraw(amount) => behavior(balance - amount)
-    case PrintBalance =>
-      println(s"balance = $balance")
-      behavior(balance)
+    case GetBalance(replyTo) =>
+      replyTo ! balance
+      Behaviors.same
   }
 
-  val actorSystem                            = ActorSystem(Behaviors.empty, name = "MyBankActorSystem")
-  val account1: ActorRef[BankAccountMessage] = actorSystem.systemActorOf(behavior(balance = 0), "account1")
-  println(account1)
+  val actorSystem = ActorSystem(SpawnProtocol(), name = "MyBankActorSystem")
 
-  account1 ! PrintBalance
-  account1 ! Deposit(200)
-  account1 ! Withdraw(50)
-  account1 ! PrintBalance
+  implicit val timeout: Timeout     = Timeout(2.seconds)
+  implicit val scheduler: Scheduler = actorSystem.scheduler
+  import actorSystem.executionContext
+
+  val bankAccountFuture: Future[ActorRef[BankAccountMessage]] =
+    actorSystem.ask[ActorRef[BankAccountMessage]] { ref =>
+      Spawn(
+        behavior = behavior(balance = 0),
+        name = "account1",
+        props = Props.empty,
+        replyTo = ref
+      )
+    }
+
+  for {
+    bankAccount <- bankAccountFuture
+    balance1    <- bankAccount ? GetBalance
+    _           = println(balance1)
+    _           = bankAccount ! Deposit(200)
+    _           = bankAccount ! Withdraw(50)
+    balance2    <- bankAccount ? GetBalance
+    _           = println(balance2)
+  } yield ()
 
   actorSystem.terminate()
 }
